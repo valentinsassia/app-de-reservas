7b6d62b409679b5faa649c4ecfeec780611ccd8c
valentinsassia <valensassia2003@outlook.com>
2023-08-26 13:24:12 -0300

    10 commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      Holaaa
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
