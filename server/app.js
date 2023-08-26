efc06125cf05b3ed069e5ad796b1533cd7855b7b
valentinsassia <valensassia2003@outlook.com>
2023-08-25 23:07:35 -0300

    8to commit

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 
 // import { dirname, join } from "path";
 // import { fileURLToPath } from "url";
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
