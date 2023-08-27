7a068acaa90384a38593cf451cb55c5a3e730cb9
valentinsassia <valensassia2003@outlook.com>
2023-08-27 19:05:21 -0300

    13 commit

---
+++ client/src/pages/components/slider.js
@@ -0,0 +1,50 @@
+import "./slider.css"
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css";
+
+function Slider() {
+  const img = [
+    "https://www.quepasaweb.com.ar/wp-content/uploads/2020/10/cancha-futbol-5-autorizada-cuarentena.jpg",
+    "https://cdn.elobservador.com.uy/102019/1570231150178.jpg"
+  ];
+
+  return (
+    <div className="swiperContenedor">
+      <Swiper
+        modules={[Pagination, Autoplay, Navigation]}
+        autoplay={{
+          delay: 2000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          el: ".pagination",
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        loop={true}
+      >
+        {img.map((imagen, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={imagen}></img>
+            </SwiperSlide>
+          );
+        })}
+        <div className="pagination"></div>
+        <div className="button swiper-button-next"></div>
+        <div className="button swiper-button-prev"></div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,14 +1,14 @@
+import Slider from "./components/slider";
 import "./home.css";
 
 function Home() {
   return (
     <div className="home">
       <div className="contenedor arriba"></div>
-      {/* <div className="contenedor menu">
-        <div className="menu_oculto"></div>
-        <div className="menu_flecha"></div>
-      </div> */}
-      <div className="contenedor medio"></div>
+      <div className="contenedor menu"></div>
+      <div className="contenedor medio">
+        {<Slider />}
+      </div>
       <div className="contenedor abajo"></div>
     </div>
   );
