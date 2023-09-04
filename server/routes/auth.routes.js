b002ac4ebfcc7922def36c7a95d13891abd3acbf
valentinsassia <valensassia2003@outlook.com>
2023-09-04 20:09:57 -0300

    51 commit

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 router.get("/hola", (req, res) => {
-    res.sendFile(join(__dirname, "./../.../client/build"))
+    res.sendFile(join(__dirname, "../../client/build"))
 })
 
 router.get("/holaa", (req, res) => {
