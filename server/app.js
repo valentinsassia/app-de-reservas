ee23300ef95e0701612c685129a7e1662e639850
valentinsassia <valensassia2003@outlook.com>
2023-09-05 16:31:47 -0300

    52 commit

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ import { fileURLToPath } from "url";
 
 import { info_complejo, reservar } from "./socket-functions.js";
 
-import rutas from "./routes/auth.routes.js";
-
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 io.on("connection", (socket) => {
   try {
@@ -26,12 +26,16 @@ io.on("connection", (socket) => {
   }
 });
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 app.use(express.json());
 
-app.use(rutas)
-
 app.use(express.static(join(__dirname, "./../client/build")));
+
+app.get("/prueba", (req,res) => {
+  res.sendFile(join(__dirname, "./../client/build"))
+})
+
+app.get("/enviar", (req, res) => {
+  res.redirect("/")
+})
 
 export default server;
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,18 +1,7 @@
 import { Router } from "express";
 const router = Router()
 
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-router.get("/hola", (req, res) => {
-    res.sendFile(join(__dirname, "../../client/build"))
-})
-
-router.get("/holaa", (req, res) => {
-    res.send("44")
-})
 
 export default router
 
